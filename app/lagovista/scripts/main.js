cadf44cf634e55d8ea814275b26cd3c6631a7482
aguilarmiqueas <aguilarmiqueas@gmail.com>
2019-07-19 21:58:18 -0300

    asd

--- app/lagovista/scripts/main.js
+++ app/lagovista/scripts/main.js
@@ -1,4 +1,22 @@
 "use strict";
+const formOverlay = document.querySelector(".form-overlay");
+const botonDeVolver = document.querySelector(".Informacion-right-volver");
+
+const botonesDeAbrir = document.querySelectorAll(".abrirPopup");
+
+let listaDeBotonesPopup = [];
+listaDeBotonesPopup.push(botonDeVolver);
+
+botonesDeAbrir.forEach(botonDeAbrir => {
+  listaDeBotonesPopup.push(botonDeAbrir);
+});
+
+listaDeBotonesPopup.forEach(botonPopup => {
+  botonPopup.addEventListener("click", function() {
+    formOverlay.classList.toggle("form-overlay-open");
+  });
+});
+
 window.onload = function() {
   $("#Galeria").slick({
     dots: true,
